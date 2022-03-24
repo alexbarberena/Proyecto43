2d4201763d0e693c1baa73c11a23669bbbb4b2da
alexbarberena <91705859+alexbarberena@users.noreply.github.com>
2022-03-23 19:55:20 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+     textSize(15);
+     fill("white")
+     text("C U N I F O N",700,130);
+     fill("lightblue");
+     text("Hint: Performs a particular task !!!",700,160);
+
+     textSize(15);
+     fill("white")
+     text("E D A T E D O S B A S",100,300);
+     fill("lightblue");
+     text("Hint: Stores all information !!!",100,330);
+
+}
